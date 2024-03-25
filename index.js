e5ca74ae3d153d37f9165515e5aecd10a5f46910
shkarzan <arzanworkspace@gmail.com>
2024-03-25 15:13:09 +0530

    2nd commit

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const express = require('express')
 // import express from express
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 app.get("/",(req,res) =>{
     res.send("Hello World")
 })
-app.get("/love",(req,res)=>{
-    res.send("<h1>Aran Loves Nasreennnnnnnnn</h1>")
+app.get("/ronaldo",(req,res)=>{
+    res.send("<h1>SUIIIIIIII</h1>")
 })
 
 app.listen(process.env.PORT, () =>{
-    console.log('Machine is Litening on ${port}')
+    console.log('Machine is Litening on http://localhost:3000',)
 })
